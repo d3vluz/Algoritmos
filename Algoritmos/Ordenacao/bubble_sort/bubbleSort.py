8b6b104039b3e36d13c8799d6deebcd2047019d9
Evandro Araújo Luz <149076775+d3vluz@users.noreply.github.com>
2024-10-25 17:02:01 -0300

    :recycle: fix: Ajuste na vizualização do BubbleSort

--- Algoritmos/Ordenacao/bubble_sort/bubbleSort.py
+++ Algoritmos/Ordenacao/bubble_sort/bubbleSort.py
@@ -3,81 +3,86 @@ import random
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-def bubbleSort(lista):
+def bubble_sort(lista):
     n = len(lista)
+    sorted_idx = n
     for i in range(n):
-        swapped = False
-        for j in range(0, n-i-1):
-            yield lista, j, j+1
-            if lista[j] > lista[j+1]:
-                lista[j], lista[j+1] = lista[j+1], lista[j]
-                swapped = True
-            yield lista, None, None
-        
-        if not swapped:
+        trocou = False
+        for j in range(n - i - 1):
+            yield lista, j, j + 1, sorted_idx
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+                trocou = True
+            yield lista, None, None, sorted_idx
+        sorted_idx -= 1
+        if not trocou:
+            sorted_idx = 0
+            yield lista, None, None, sorted_idx
             break
 
-def visualize_bubble_sort(lista):
+def visualizar_bubble_sort(lista):
     N = len(lista)
     fig, ax = plt.subplots()
     ax.set_title("Bubble Sort")
-    bar_rects = ax.bar(range(N), lista, align="edge", color='blue')
+    barras = ax.bar(range(N), lista, align="edge", color='blue')
 
     ax.set_xlim(0, N)
     ax.set_ylim(0, int(1.1 * N))
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
-    text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
-    iteration = [0]
+    contador = ax.text(0.02, 0.95, "", transform=ax.transAxes)
+    operacoes = [0]
 
-    def update_fig(data, rects, iteration):
-        lista, x, y = data
-        for rect, val in zip(rects, lista):
-            rect.set_height(val)
-            rect.set_color('blue')
+    def atualizar_grafico(data, barras, operacoes):
+        lista, x, y, sorted_idx = data
+        for i, (barra, valor) in enumerate(zip(barras, lista)):
+            barra.set_height(valor)
+            if i >= sorted_idx:
+                barra.set_color('green')
+            else:
+                barra.set_color('blue')
         if x is not None and y is not None:
-            rects[x].set_color('purple')
-            rects[y].set_color('purple')
-        iteration[0] += 1
-        text.set_text("Número de operações: {}".format(iteration[0]))
+            barras[x].set_color('purple')
+            barras[y].set_color('purple')
+        operacoes[0] += 1
+        contador.set_text(f"Número de Operações: {operacoes[0]}")
 
-    anim = animation.FuncAnimation(fig, func=update_fig,
-                                   fargs=(bar_rects, iteration), frames=bubbleSort(lista), interval=50,
-                                   repeat=False)
+    animacao = animation.FuncAnimation(fig, func=atualizar_grafico,
+                                       fargs=(barras, operacoes), frames=bubble_sort(lista),
+                                       interval=200, repeat=False)
     plt.show()
 
-
-def get_sorted_data(N):
+def interface(N):
     while True:
-        print("Escolha o caso para a ordenação Bubble Sort:\n")
-        print("1 - Melhor caso")
-        print("2 - Pior caso")
-        print("3 - Caso médio")
+        print("\nEscolha o caso para a ordenação Bubble Sort:")
+        print("1 - Lista Ordenada")
+        print("2 - Lista Ordenada Inversamente")
+        print("3 - Lista Aleatoria")
         print("4 - Sair")
-        case = input("\nEscolha -> ").strip().lower()
         
-        if case == '1' or case == "melhor caso":
-            return list(range(N))
-        elif case == '2' or case == "pior caso":
+        opcao = input("\nEscolha -> ").strip().lower()
+        
+        if opcao in ['1', 'lista ordenada']:
+            return list(range(1, N+1))
+        elif opcao in ['2', 'lista ordenada inversamente']:
             return list(range(N, 0, -1))
-        elif case == '3' or case == "caso médio":
-            lista = list(range(N))
+        elif opcao in ['3', 'lista aleatoria']:
+            lista = list(range(1, N+1))
             random.shuffle(lista)
             return lista
-        elif case == '4' or case == "sair":
+        elif opcao in ['4', 'sair']:
             print("Saindo do programa.")
             return None
         else:
-            print("Escolha inválida. Por favor, tente novamente.\n")
+            print("Opção inválida. Tente novamente.")
 
 if __name__ == "__main__":
     N = 20
     while True:
-        os.system('cls')
-        lista = get_sorted_data(N)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        lista = interface(N)
         if lista is not None:
-            visualize_bubble_sort(lista)
+            visualizar_bubble_sort(lista)
         else:
-            print("O programa foi encerrado pelo usuário.")
             break
