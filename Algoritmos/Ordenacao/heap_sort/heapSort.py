fbc79a21fd2a2124023d2fae8901f2e0b734d3ac
Evandro Araújo Luz <149076775+d3vluz@users.noreply.github.com>
2024-11-07 18:13:54 -0300

    :recycle: refactor: Novo Algoritmo HeapSort

--- Algoritmos/Ordenacao/heap_sort/heapSort.py
+++ Algoritmos/Ordenacao/heap_sort/heapSort.py
@@ -1,53 +1,104 @@
-def heapify(arr, n, i):
-  """
-  Função para transformar uma subárvore em um heap máximo.
+import os
+import random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-  Args:
-      arr: Lista a ser ordenada.
-      n: Tamanho do heap.
-      i: Índice da raiz da subárvore.
-  """
-  largest = i  # Inicializa o maior como raiz
-  l = 2 * i + 1  # Filho esquerdo
-  r = 2 * i + 2  # Filho direito
+def heap_sort(lista):
+    n = len(lista)
 
-  # Verifica se o filho esquerdo existe e é maior que a raiz
-  if l < n and arr[largest] < arr[l]:
-    largest = l
+    for i in range(n // 2 - 1, -1, -1):
+        yield from heapify(lista, n, i)
 
-  # Verifica se o filho direito existe e é maior que a raiz
-  if r < n and arr[largest] < arr[r]:
-    largest = r
+    for i in range(n - 1, 0, -1):
+        lista[i], lista[0] = lista[0], lista[i]
+        yield lista, 0, i, i
+        yield from heapify(lista, i, 0)
+    
+    yield lista, -1, -1, 0
 
-  # Se o maior não for a raiz
-  if largest != i:
-    arr[i], arr[largest] = arr[largest], arr[i]  # Troca a raiz
+def heapify(lista, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
 
-    # Recursivamente, transforma a subárvore afetada em um heap
-    heapify(arr, n, largest)
+    if left < n and lista[i] < lista[left]:
+        largest = left
 
+    if right < n and lista[largest] < lista[right]:
+        largest = right
 
-def heap_sort(arr):
-  """
-  Função para realizar a ordenação heapsort.
+    if largest != i:
+        lista[i], lista[largest] = lista[largest], lista[i]
+        yield lista, i, largest, n
+        yield from heapify(lista, n, largest)
 
-  Args:
-      arr: Lista a ser ordenada.
-  """
-  n = len(arr)
+def visualize_heap_sort(lista):
+    N = len(lista)
+    fig, ax = plt.subplots()
+    ax.set_title("Heap Sort")
+    bar_rects = ax.bar(range(N), lista, align="edge", color='blue')
+    ax.set_xlim(0, N)
+    ax.set_ylim(0, int(1.1 * N))
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
+    iteration = [0]
+    ordered_indices = set()
 
-  # Constrói um heap máximo
-  for i in range(n // 2 - 1, -1, -1):
-    heapify(arr, n, i)
+    def update_fig(data, rects, iteration):
+        lista, index1, index2, end_sorted = data
+        for rect, val in zip(rects, lista):
+            rect.set_height(val)
 
-  # Extrai elementos do heap, um por vez
-  for i in range(n - 1, 0, -1):
-    arr[i], arr[0] = arr[0], arr[i]  # Troca o elemento raiz (maior) com o último elemento
-    heapify(arr, i, 0)  # Chama o heapify na subárvore reduzida
+        for i in range(N):
+            if i >= end_sorted:
+                rects[i].set_color('green')
+                ordered_indices.add(i)
+            elif i not in ordered_indices:
+                rects[i].set_color('blue')
 
+        if index1 != -1 and index2 != -1:
+            rects[index1].set_color('purple')
+            rects[index2].set_color('purple')
 
-# Exemplo de uso:
-arr = [12, 11, 13, 5, 6, 7]
-print(arr)
-heap_sort(arr)
-print(arr)
+        iteration[0] += 1
+        text.set_text("Número de operações: {}".format(iteration[0]))
+
+    anim = animation.FuncAnimation(fig, func=update_fig,
+                                   fargs=(bar_rects, iteration), frames=heap_sort(lista), interval=200,
+                                   repeat=False)
+    plt.show()
+
+def get_sorted_data(N):
+    while True:
+        print("Escolha o caso para a ordenação Heap Sort:\n")
+        print("1 - Lista Ordenada")
+        print("2 - Lista Ordenada Inversamente")
+        print("3 - Lista Aleatoria")
+        print("4 - Sair")
+        case = input("\nEscolha -> ").strip().lower()
+        
+        if case == '1' or case == "lista ordenada":
+            return list(range(1, N+1))
+        elif case == '2' or case == "lista ordenada inversamente":
+            return list(range(N, 0, -1))
+        elif case == '3' or case == "lista aleatoria":
+            lista = list(range(1, N+1))
+            random.shuffle(lista)
+            return lista
+        elif case == '4' or case == "sair":
+            print("Saindo do programa.")
+            return None
+        else:
+            print("Escolha inválida. Por favor, tente novamente.\n")
+
+if __name__ == "__main__":
+    N = 20
+    while True:
+        os.system('cls')
+        lista = get_sorted_data(N)
+        if lista is not None:
+            visualize_heap_sort(lista)
+        else:
+            print("O programa foi encerrado pelo usuário.")
+            break
